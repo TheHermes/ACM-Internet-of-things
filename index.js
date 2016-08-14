b15914b1778587ef6875863bfbf240a6a6462bcd
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-14 15:27:08 +0430

    Improves the code

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var gpio = require('rpi-gpio');
 gpio.on('change', function(channel, value) {
         console.log('Channel ' + channel + ' value is now ' + value);
 });
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(8, gpio.DIR_IN, gpio.EDGE_BOTH);
