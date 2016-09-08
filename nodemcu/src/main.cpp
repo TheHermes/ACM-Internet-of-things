13d7bdac3c37937c166742e9b3233b97866103f8
Iman Tabrizian <Tabrizian@outlook.com>
2016-09-08 21:48:58 +0430

    Adds report of the humidity

--- nodemcu/src/main.cpp
+++ nodemcu/src/main.cpp
@@ -25,34 +25,36 @@ void setup()
 
 void loop()
 {
-  WiFiClient client;
+    double humidity = analogRead(A0);
+    Serial.println((humidity));
+  //WiFiClient client;
 
-  Serial.printf("\n[Connecting to %s ... ", host);
-  if (client.connect(host, 3000))
-  {
-    Serial.println("connected]");
+  //Serial.printf("\n[Connecting to %s ... ", host);
+  //if (client.connect(host, 3000))
+  //{
+  //  Serial.println("connected]");
 
-    Serial.println("[Sending a request]");
-    char *request = "GET /goldoon HTTP/1.1\n Host: 192.168.1.6\n Cache-Control: no-cache\n\n";
-    Serial.println(request);
-    client.print(request);
+  //  Serial.println("[Sending a request]");
+  //  char *request = "GET /goldoon HTTP/1.1\n Host: 192.168.1.6\n Cache-Control: no-cache\n\n";
+  //  Serial.println(request);
+  //  client.print(request);
 
-    Serial.println("[Response:]");
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        String line = client.readStringUntil('\n');
-        Serial.println(line);
-      }
-    }
-    client.stop();
-    Serial.println("\n[Disconnected]");
-  }
-  else
-  {
-    Serial.println("connection failed!]");
-    client.stop();
-  }
-  delay(1000);
+  //  Serial.println("[Response:]");
+  //  while (client.connected())
+  //  {
+  //    if (client.available())
+  //    {
+  //      String line = client.readStringUntil('\n');
+  //      Serial.println(line);
+  //    }
+  //  }
+  //  client.stop();
+  //  Serial.println("\n[Disconnected]");
+  //}
+  //else
+  //{
+  //  Serial.println("connection failed!]");
+  //  client.stop();
+  //}
+  //delay(1000);
 }
